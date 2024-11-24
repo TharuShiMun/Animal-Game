94e91eced09bc4c8ea0915bea7091eb357ffc423
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2024-11-24 03:14:06 +0000

    Modified App.js file

---
+++ animalgame/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import GameTable from "./components/GameTable";
+
+function App() {
+  return (
+    <div className="App">
+     {/* Add game table component */}
+    <GameTable/>
+    </div>
+  );
+}
+
+export default App;
+ 
